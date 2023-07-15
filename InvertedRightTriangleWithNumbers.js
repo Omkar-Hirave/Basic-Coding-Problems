36baa6bf38e93f4ddf2458c4c8da434ff4deb455
oi-OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-07-15 07:42:21 +0530

    Add files via upload

---
+++ InvertedRightTriangleWithNumbers.js
@@ -0,0 +1,13 @@
+function InvertedRightTriangleWithNumbers(num){
+    let string = ''
+    
+    for(let i = 0 ; i < num ; i++){ 
+        for(let j = i + 1 ; j <= num ; j++){
+            string = string + (i+1)
+        }
+        string = string + '\n'
+    }
+    return string 
+}
+let num = 5
+console.log(InvertedRightTriangleWithNumbers(num));
