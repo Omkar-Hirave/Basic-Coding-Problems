7e0cc294475f326bf7dc01069b36f55f0acee9e9
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-07-24 10:47:36 +0530

    Add files via upload

---
+++ SquarePattern.js
@@ -0,0 +1,12 @@
+function squarePattern(num){
+    let string =''
+    for(let i = 0 ; i < num ; i++){
+        for(let j = i + 1  ; j < num ; j++){
+            string = string + '*'
+        }
+        string = string + '\n'
+    }
+    return string
+}
+let num = 5
+console.log(squarePattern(num));
