0725843d618c985e6f05e47e019b657ce6a04d80
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-07-28 08:53:44 +0530

    Add files via upload

---
+++ DiamondPatternWithSameNumbers.js
@@ -0,0 +1,17 @@
+function DiamondPatternWithSameNumbers(num){
+    for(let i = 0 ; i < num ; i++){
+        let string = ''
+            for(let j = 0 ; j < num - i - 1 ; j++){
+                string = string + '.'
+            }
+            for(let j = 0 ; j < 2 * i + 1 ; j++){
+                string = string + (i+1)
+            }
+            for(let j = 0 ; j < num - i - 1 ; j++){
+                string = string + '.'
+            }
+            console.log(string);
+        }
+    }
+let num = 5 
+DiamondPatternWithSameNumbers(num)
